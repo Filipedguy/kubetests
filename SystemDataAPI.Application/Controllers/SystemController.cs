622281e3276cd34bfaa2de415809f7b0070101e2
Filipe <filipecunhavasconcellos@hotmail.com>
2018-08-29 09:17:47 -0300

    Create system controller

---
+++ SystemDataAPI.Application/Controllers/SystemController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SystemDataAPI.Application.Controllers
+{
+    [Route("system")]
+    public class SystemController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var systemInfo = new
+            {
+                SystemPlatform = Environment.OSVersion.Platform.ToString(),
+                SystemVersion = Environment.OSVersion.Version.ToString(),
+                HostName = Environment.MachineName
+            };
+
+            return Ok(systemInfo);
+        }
+
+        [HttpGet("status/{id}")]
+        public IActionResult GetStatus(int id)
+        {
+            if (id == 0)
+            {
+                return StatusCode(500, "O sistema ta zuado");
+            }
+
+            return Ok("Tudo ok por aqui");
+        }
+    }
+}
